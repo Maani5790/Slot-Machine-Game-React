01f80250da50888e7637e095be17e6a85554da09
Abdul Rehman <114314272+Maani5790@users.noreply.github.com>
2022-12-17 20:48:49 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+const SlotM = (props) => {
+  // let x = "😀";
+  // let y = "😀";
+  // let z = "😋";
+
+  let x = props.x;
+  let y = props.y;
+  let z = props.z;
+
+  if (x === y && y === z) {
+    return (
+      <>
+        <div className="slot_inner">
+          <h1>
+            {x} {y} {z}
+          </h1>
+          <h1>This is matching</h1>
+          <hr />
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="slot_inner">
+          <h1>
+            {x} {y} {z}
+          </h1>
+          <h1>This is not matching</h1>
+          <hr />
+        </div>
+      </>
+    );
+  }
+};
+
+const App = () => {
+  return (
+    <>
+      <h1 className="heading_style">
+        🎰 welcome to{" "}
+        <span style={{ fontWeight: "bold" }}>Slot Machine Game 🎰</span> {""}
+      </h1>
+      <div className="slotMachine">
+      <SlotM x='😀'  y='😀' z='😀' />
+      <SlotM x='🍞' y='🥠' z='🍿' />
+      <SlotM  x='🍗' y='🥚'  z='🍟' />
+        
+
+      </div>
+    </>
+  );
+};
+
+export default App;
